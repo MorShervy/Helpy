f936fc1bbfd053aab2961454e12bea6faba22e03
MorShervy <morshervy@gmail.com>
2019-06-21 08:40:46 +0300

    dd

--- helpy-admin/src/screens/main/Dashboard.jsx
+++ helpy-admin/src/screens/main/Dashboard.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
 
 
 export default function Dashboard(props) {
@@ -14,9 +22,10 @@ export default function Dashboard(props) {
             <Typography component="div" style={{ backgroundColor: '#f6f8fa', height: '100vh' }} >
 
                 <Box display="flex" justifyContent="flex-start" flexDirection="row" m={0} p={2}>
-                    <Box display="flex" p={4}>
+                    <Box display="flex" flexGrow={0} p={4}>
                         <Box>
                             <Headers txt='Dashbaord' />
+                            <TypographyMenu />
                         </Box>
                     </Box>
 
@@ -40,11 +49,48 @@ export function Headers(props) {
         <div style={{ backgroundImage: 'linear-gradient(#358FE2, #2C0A8C)' }}>
             <div style={{ flexGrow: 1, }}>
                 <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
-                    <Typography variant="h6" style={{ flexGrow: 1, }}>
+                    <Typography variant="h6" style={{ flexGrow: 1, paddingLeft: 10 }}>
                         {props.txt}
                     </Typography>
                 </AppBar>
             </div>
         </div>
     )
+}
+
+const useStyles = makeStyles({
+    root: {
+        width: 230,
+    },
+});
+
+export function TypographyMenu() {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <MenuList>
+                <MenuItem>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <Typography variant="inherit">A short message</Typography>
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <PriorityHighIcon />
+                    </ListItemIcon>
+                    <Typography variant="inherit">A very long text that overflows</Typography>
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <DraftsIcon />
+                    </ListItemIcon>
+                    <Typography variant="inherit" noWrap>
+                        A very long text that overflows
+            </Typography>
+                </MenuItem>
+            </MenuList>
+        </Paper>
+    );
 }
